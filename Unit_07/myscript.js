01d37ced4817159f9fb3feb8d73b80f965551529
Daniel Niven <1407780@dundeeandangus.ac.uk>
2021-11-12 12:11:39 +0000

    Updated event + myscript

---
+++ Unit_07/myscript.js
@@ -0,0 +1,10 @@
+let headerText = document.getElementByID('id1');
+
+function orangeButton() {
+  headerText.style.color = 'orange';
+}
+
+
+function goAgain(){
+    headerText.style.color = 'yellow';
+}
